1a3e305edc5e8fca36dff259a0650ffc6da38cbf
Coto Cisternas <cotocisternas@gmail.com>
2017-02-09 16:34:36 -0300

    push

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,18 +2,18 @@
 #
 class fluentd::config inherits fluentd {
 
-  file { $::fluentd::config_file:
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    source => 'puppet:///modules/fluentd/td-agent.conf',
-    notify => Class['Fluentd::Service'],
-  }
+#  file { $::fluentd::config_file:
+#    ensure => file,
+#    owner  => 'root',
+#    group  => 'root',
+#    source => 'puppet:///modules/fluentd/td-agent.conf',
+#    notify => Class['Fluentd::Service'],
+#  }
 
-  file { $::fluentd::conf_dir:
-    ensure => 'directory',
-    owner  => $::fluentd::user_name,
-    group  => $::fluentd::user_group,
-    mode   => '0750',
-  }
+#  file { $::fluentd::conf_dir:
+#    ensure => 'directory',
+#    owner  => $::fluentd::user_name,
+#    group  => $::fluentd::user_group,
+#    mode   => '0750',
+#  }
 }
